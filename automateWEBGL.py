e7f442d941b95c3423fa18a1822d2b324b8f9b70
Sachin <sachx2.0@hotmail.com>
2020-05-20 15:36:43 -0400

    automated html creator

---
+++ automateWEBGL.py
@@ -0,0 +1,11 @@
+name = input('What would you like to name your file? ')
+
+f = open(name, 'w')
+
+message = """<html>
+<head></head>
+<body><p>Hello World!</p></body>
+</html>"""
+
+f.write(message)
+f.close()
