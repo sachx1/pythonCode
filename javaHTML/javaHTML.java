71d6f27b6d1cbc44a14a4f5d255bf74ce58b935b
Sachin <sachx2.0@hotmail.com>
2020-11-19 12:19:06 -0500

    updates to java html generator

--- javaHTML/javaHTML.java
+++ javaHTML/javaHTML.java
@@ -23,7 +23,6 @@ public class javaHTML {
         System.out.println("Name of the heartpiece ");
         myObj.nextLine(); //creates line so program does not skip
         String name1 = myObj.nextLine();
-
         String name2 = myObj.nextLine();  
         String name3 = myObj.nextLine(); 
 
@@ -62,6 +61,70 @@ public class javaHTML {
             bw.write(" <title>2018017-01 High Resolution</title>" + "\n" + 
             "<meta charset=\"utf-8\">" + "\n" + 
             "<meta name=\"viewport\" content=\"width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0\">");
+            bw.write("<style>" + "\n" + 
+            "@import url('https://fonts.googleapis.com/css2?family=Krona+One&display=swap');" + "\n" +
+            "body {" + "\n" + 
+               "/*background-color: #ccc;*/" + "\n" + 
+                "background-color: black;" + "\n" + 
+                "color: #000;" + "\n" + "}");
+            bw.write("\n" + "\n" + "\t" + "a {" + "\n" + 
+                "\t" + "\t" + "color: #f00;" + "\n" + 
+            "}");
+            bw.write("\n" + "\n" + ".overlay {" + "\n" + 
+                "height: 100%;" + "\n" + 
+                "width: 0;" + "\n" + 
+                "position: fixed;" + "\n" + 
+                "z-index: 1;" + "\n" + 
+                "top: 0;" + "\n" + 
+                "left: 0;" + "\n" + 
+                "background-color: rgb(0,0,0);" + "\n" + 
+                "background-color: rgba(0,0,0, 0.9);" + "\n" + 
+                "overflow-x: hidden;" + "\n" + 
+                "transition: 0.5s;" + "\n" + 
+                "}");
+            bw.write("\n" + "\n" + ".overlay-content {" + "\n" + 
+                "position: relative;" + "\n" +
+                "top: 5%;" + "\n" + 
+                "width: 100%;" + "\n" + 
+                "text-align: center;" + "\n" + 
+                "margin-top: 30px;" + "\n" + 
+                "color: white;" + "\n" + 
+                "}");
+            bw.write("\n" + "\n" + ".overlay a {" + "\n" + 
+                "padding: 8px;" + "\n" + 
+                "text-decoration: none;" + "\n" + 
+                "font-size: 36px;" + "\n" + 
+                "color: #818181;" + "\n" + 
+                "display: block;" + "\n" + 
+                "transition: 0.3s;" + "\n" + 
+                "}");
+            bw.write("\n" + "\n" + ".overlay a:hover, .overlay a:focus {" + "\n" + 
+                "color: #f1f1f1;" + "\n" + 
+                "}");
+            bw.write(".overlay .closebtn {" + "\n" + 
+                "position: absolute;" + "\n" + 
+                "top: 20px;" + "\n" + 
+                "right: 45px;" + "\n" + 
+                "font-size: 60px;" + "\n" +
+                "}");
+            bw.write("\n" + "\n" + "@media screen and (max-height: 450px) {" + "\n" + 
+                ".overlay a {font-size: 20px}" + "\n" + 
+                ".overlay .closebtn {" + "\n" + 
+                "font-size: 40px;" + "\n" + 
+                "top: 15px;" + "\n" + 
+                "right: 35px;" + "\n" + 
+                "}" + "\n" + 
+                "}");
+            
+            bw.write("\n" + "\n" + ".sidebar{" + "\n" + 
+                "position: fixed;" + "\n" + 
+                "width: 200px;" + "\n" + 
+                "top:0;" + "\n" + 
+                "left: 0;" + "\n" + 
+                "bottom: 0;" + "\n" + 
+                "background: grey;" + "\n" + 
+                "padding-top: 50px;" + "\n" + 
+                "}");
             bw.close();
             System.out.println("Successfully wrote to the file.");
           } catch (IOException e) {
